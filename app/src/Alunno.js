cbf29f98fe457d801ca7146002569f67083fdf7d
lai2-24 <lai2-24@itismeucci.com>
2024-04-29 11:32:56 +0200

    Final Commit

--- app/src/Alunno.js
+++ app/src/Alunno.js
@@ -3,7 +3,13 @@ import {useState} from 'react';
 export default function Alunno({alunno, loadAlunni}){
 
     const [inCancellazione, setInCancellazione] = useState(false);
+    const [inMod, setInMod] = useState(false);
+
     const [inConferma, setInConferma] = useState(false);
+    const [inModifica, setInModifica] = useState(false);
+
+    const [nome, setNome] = useState("");
+    const [cognome, setCognome] = useState("");
 
     async function cancellaAlunno(){
         setInCancellazione(true);
@@ -11,15 +17,40 @@ export default function Alunno({alunno, loadAlunni}){
         loadAlunni();
         setInCancellazione(false);
     }
+    async function modificaAlunno(){
+        setInMod(true);
+        await fetch(`http://localhost:8080/alunni/${alunno.id}`, 
+        {method: "PUT",
+            headers: {'Content-Type' : 'application/json'},
+            body: JSON.stringify({nome: nome, cognome: cognome})
+        });
+        loadAlunni();
+        setInMod(false);
+    }
     function richiediConferma(){
         setInConferma(true);
+    }
+    function richiediModifica(){
+        setInModifica(true);
+    }
+    function annullaModifica(){
+        setInModifica(false);
     }
     function annulla(){
         setInConferma(false);
     }
+    function gestisciCambioNome(e){
+        setNome(e.target.value);
+    }
+      function gestisciCambioCognome(e){
+        setCognome(e.target.value);
+    }
     return(
         <div>
-            {alunno.id} - {alunno.nome} {alunno.cognome} -
+            {alunno.id} 
+            {!inModifica &&
+                <>  - {alunno.nome} {alunno.cognome} -</>
+            }
             { inCancellazione ?
                 <span> in cancellazione... </span>
             :
@@ -29,7 +60,34 @@ export default function Alunno({alunno, loadAlunni}){
                         <button onClick={cancellaAlunno}>si</button>
                         <button onClick={annulla}>no</button></span>
                         :
-                        <button onClick={richiediConferma}>Cancella alunno</button>
+                        <span>
+                            { !inModifica &&
+                                <button onClick={richiediConferma}>Cancella alunno</button>
+                            }
+                        </span>
+                    }
+                </span>
+            }
+            { inMod ?
+                <span> in modifica... </span>
+                :
+                <span>
+                    { inModifica ?
+                        
+                        <>
+                        <input type='text' placeholder='Inserisci il nome' onChange={gestisciCambioNome} value={nome}></input>
+                        <input type='text' placeholder='Inserisci il cognome' onChange={gestisciCambioCognome} value={cognome}></input>
+                        <span>Vuoi modificare?
+                        <button onClick={modificaAlunno}>si</button>
+                        <button onClick={annullaModifica}>no</button></span>
+                        </>
+                        :
+                        <span>
+                            { !inConferma &&
+                                <button onClick={richiediModifica}>Modifica alunno</button>
+                            }
+                        </span>
+
                     }
                 </span>
             }
--- app/src/App.js
+++ app/src/App.js
@@ -5,8 +5,13 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [alunni, setAlunni] = useState([]);
-  const[inCaricamento, setInCaricamento] = useState(false);
+  const [inCaricamento, setInCaricamento] = useState(false);
+  const [showForm, setShowForm] = useState(false);
 
+  const [nome, setNome] = useState("");
+  const [cognome, setCognome] = useState("");
+
+  
 
   useEffect(() => {
     loadAlunni();  
@@ -19,6 +24,26 @@ function App() {
     setAlunni(a);
     setInCaricamento(false);
   };
+  /*function mostraForm(){
+    setShowForm(true);
+  }*/
+  async function salvaAlunno(){
+    await fetch(`http://localhost:8080/alunni`, 
+    {
+      method: "POST",
+      headers: {'Content-Type' : 'application/json'},
+      body: JSON.stringify({nome: nome, cognome: cognome})
+    }
+    );
+    loadAlunni();
+    setShowForm(false);
+  }
+  function gestisciCambioNome(e){
+    setNome(e.target.value);
+  }
+  function gestisciCambioCognome(e){
+    setCognome(e.target.value);
+  }
 
   return (
     <div className="App">
@@ -31,6 +56,18 @@ function App() {
           alunni.map((alunno) => (
             <Alunno alunno={alunno} loadAlunni={loadAlunni}  key={alunno.id} />
           ))
+      }
+
+      <button onClick={() => setShowForm(true)}>Inserisci nuovo alunno</button>
+      { showForm &&
+        <div>
+          <h1>Form di inserimento</h1>
+          <div>Nome: <input type='text' placeholder='Inserisci il nome' onChange={gestisciCambioNome} value={nome}></input></div>
+          <div>Cognome: <input type='text' placeholder='Inserisci il cognome' onChange={gestisciCambioCognome} value={cognome}></input></div>
+          <button onClick={salvaAlunno}>Salva</button>
+          <button onClick={() => setShowForm(false)}>Annulla</button>
+          <div>{nome}{cognome}</div>
+        </div>
       }
     </div>
   );
